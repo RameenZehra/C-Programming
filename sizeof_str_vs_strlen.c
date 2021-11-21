5bb8362af9f496ab44ff380370025b0ac6041bad
RameenZehra <rameenzaidi786@gmail.com>
2021-11-21 13:05:44 +0500

    sizeof() vs strlen()
    Comparison of sizeof() and strlen() used to find out the length of a string.

---
+++ sizeof_str_vs_strlen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main()
+{
+    char str[] = "Hello";
+    //olleH
+    printf("size of str is %d\n", sizeof(str));
+    printf("len of str is %d\n", strlen(str));
+
+
+
+    return 0;
+}
