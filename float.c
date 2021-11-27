41d3e1cb8b8c72d82c1addd796a2f31a915ccb7a
RameenZehra <rameenzaidi786@gmail.com>
2021-11-27 13:24:01 +0500

    float_pointer
    stores the address of a float variable

---
+++ float.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    float c = 10.75;
+    float * ptr = &c;
+    printf("address of c is : %x\n", ptr);
+    printf("address of c is : %x\n", ptr);
+
+    return 0;
+}
